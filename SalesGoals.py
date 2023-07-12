303b9e4de9a29dba1d62b980ec19b8ff7ef0465d
Elizabeth Innemee <55023647+einnemee@users.noreply.github.com>
2023-07-12 08:06:35 +0100

    Update SalesGoals.py
    Weekly goals added

--- SalesGoals.py
+++ SalesGoals.py
@@ -2,14 +2,21 @@
 
 initialSalesGoal = 20000
 multiplier = 100
+offMonth = True
 
-for monthlyGoal in range (1,13):
-
-    if monthlyGoal == 6:
-        print ('No goal for month 6')
-        continue
+for monthlyGoal in range(1,13):
     
+    if monthlyGoal == 6 and offMonth == True:
+        print("No goal for month 6")
+        continue
+        
     monthlySalesGoal = initialSalesGoal + (monthlyGoal * multiplier)
-    print ("Your sales goal for month ", monthlyGoal, "is ", monthlySalesGoal)
 
-print ('Good luck with you goals')
+    print("Your sales goal for month " + str(monthlyGoal) + " is " + str(monthlySalesGoal))
+
+    for weeklyGoal in range (1,5):
+        
+        weeklySalesGoal = monthlySalesGoal // 4
+        print ('Your goal for week ', weeklyGoal, 'is ', weeklySalesGoal)
+    
+print("Good luck with your goals.")
