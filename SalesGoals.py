4781bf241de06a6c699021091e03f25e04c7bbdb
Elizabeth Innemee <55023647+einnemee@users.noreply.github.com>
2023-07-12 07:51:26 +0100

    Update SalesGoals.py
    Continue statement added for month 6

--- SalesGoals.py
+++ SalesGoals.py
@@ -4,6 +4,11 @@ initialSalesGoal = 20000
 multiplier = 100
 
 for monthlyGoal in range (1,13):
+
+    if monthlyGoal == 6:
+        print ('No goal for month 6')
+        continue
+    
     monthlySalesGoal = initialSalesGoal + (monthlyGoal * multiplier)
     print ("Your sales goal for month ", monthlyGoal, "is ", monthlySalesGoal)
 
